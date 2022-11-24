7a34c332d9740148b381539af9b0a282914b1591
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-11-24 17:19:19 +0100

    lyuboe sobwenie

---
+++ bg.js
@@ -0,0 +1,13 @@
+
+
+  let btn = document.querySelector('button');
+  let screenSize = document.querySelector('#screenSize');
+  let windowSize = document.querySelector('#windowSize');
+  let contentSize = document.querySelector('#contentSize');
+
+  btn.addEventListener('click', () => {
+    screenSize.innerText = `Screen height: ${screen.height}. Screen width: ${screen.width}.`
+    windowSize.innerText = `window height: ${window.outerHeight}. window width: ${window.outerWidth}.`
+    contentSize.innerText = `content height: ${window.innerHeight}. Content width: ${window.innerWidth}.`
+    
+  })
