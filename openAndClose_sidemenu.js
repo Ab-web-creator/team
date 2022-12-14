d5c2adfbcfeaf97528f8634b50bfd0b6b58e74a6
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2022-12-14 17:34:46 +0100

    lyuboe sobwenie

--- openAndClose_sidemenu.js
+++ openAndClose_sidemenu.js
@@ -1,23 +1,33 @@
 // const main = document.querySelector(".main-1");
 const menu = document.querySelector("#menuBtn");
-const menu2 = document.querySelector("#dmenuBtn");
 const cross = document.querySelector("#close_sidemenu");
 const goHome = document.querySelector("#home_sidemenu");
 const blurs = document.querySelector(".blur");
 const side = document.querySelector("aside");
 
 
+// menu.addEventListener("click", () => {
+//     console.log("Menu is clicked");
+//     blurs.classList.add("active");
+//     side.classList.add("active");
+//     // figure.classList.add("active");
+// })
+
 menu.addEventListener("click", () => {
-    console.log("Menu is clicked");
-    blurs.classList.add("active");
-    side.classList.add("active");
-    // figure.classList.add("active");
+    setTimeout(() => {
+        side.classList.add("active");
+    }, 50)
+
+    setTimeout(() => {
+        blurs.classList.add("active");
+    }, 51)
 })
 
 cross.addEventListener("click", () => {
-    blurs.classList.remove("active");
-    side.classList.remove("active");
-    // figure.className = '';
+    setTimeout(() => {
+        blurs.classList.remove("active");
+        side.classList.remove("active");
+    }, 51)
 })
 
 goHome.addEventListener("click", () => {
@@ -27,16 +37,7 @@ goHome.addEventListener("click", () => {
     window.open("../index.html", "_self")
 })
 
-menu2.addEventListener("mouseover", () => {
-    setTimeout(() => {
-        side.classList.add("active");
-        figure.classList.add("active");
-    }, 500)
 
-    setTimeout(() => {
-        blurs.classList.add("active");
-    }, 1000)
-})
 
 
 blurs.addEventListener("click", () => {
