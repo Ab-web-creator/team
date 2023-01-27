631d162124799c2da091a6b3026e4adb5c38cab3
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2023-01-27 16:02:42 +0100

    lyuboe sobwenie

--- click_to_green.js
+++ click_to_green.js
@@ -1,34 +1,15 @@
+const  waar_of_niet_waar  = (target, color="pink") => {
+ const button = document.querySelector(target)
+ button.addEventListener("click", () => {
+    button.style.background = color
+ })
+}
 
-const waar_btn1 = document.querySelector(".red_green1");
-const waar_btn2 = document.querySelector(".red_green2");
-const waar_btn3 = document.querySelector(".red_green3");
-const waar_btn4 = document.querySelector(".red_green4");
-const waar_btn5 = document.querySelector(".red_green5");
-const waar_btn6 = document.querySelector(".red_green6");
-const waar_btn7 = document.querySelector(".red_green7");
-const waar_btn8 = document.querySelector(".red_green8");
-
-waar_btn1.addEventListener("click", () => {
-    waar_btn1.style.background = "lightgreen";
-})
-waar_btn2.addEventListener("click", () => {
-    waar_btn2.style.background = "red";
-})
-waar_btn3.addEventListener("click", () => {
-    waar_btn3.style.background = "lightgreen";
-})
-waar_btn4.addEventListener("click", () => {
-    waar_btn4.style.background = "red";
-})
-waar_btn5.addEventListener("click", () => {
-    waar_btn5.style.background = "red";
-})
-waar_btn6.addEventListener("click", () => {
-    waar_btn6.style.background = "lightgreen";
-})
-waar_btn7.addEventListener("click", () => {
-    waar_btn7.style.background = "lightgreen";
-})
-waar_btn8.addEventListener("click", () => {
-    waar_btn8.style.background = "red";
-})
+waar_of_niet_waar(".red_green1", "lightgreen")
+waar_of_niet_waar(".red_green2", "red")
+waar_of_niet_waar(".red_green3", "lightgreen")
+waar_of_niet_waar(".red_green4", "red")
+waar_of_niet_waar(".red_green5", "red")
+waar_of_niet_waar(".red_green6", "lightgreen")
+waar_of_niet_waar(".red_green7", "lightgreen")
+waar_of_niet_waar(".red_green8", "red")
