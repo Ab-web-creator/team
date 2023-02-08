4c33949beb6ed258113397ce09c45c52bdbfe9c0
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2023-02-08 20:35:44 +0100

    lyuboe sobwenie

---
+++ JS_folder/drag&drop.js
@@ -0,0 +1,43 @@
+function allowDrop(div) {
+    div.preventDefault();
+  }
+  function drag(div) {
+    // console.log(div.target.id);
+    // console.log(div.dataTransfer);
+
+    div.dataTransfer.setData('text', div.target.id);
+  }
+  async function drop(div) {
+    if (div.target.id != 'div1') {
+      return;
+    }
+
+    console.log(div.target.id);
+    div.preventDefault();
+
+    let isOccupied = false;
+
+    var array = Array.prototype.slice.call(div.target.children);
+
+    // console.log('array', array);
+
+    await array.forEach(async (element) => {
+      // console.log(element.getAttribute('draggable'));
+
+      if (element.getAttribute('draggable')) {
+        // console.log(element);
+        isOccupied = true;
+        // console.log(isOccupied);s
+      }
+    });
+
+    // console.log(isOccupied);
+
+    if (isOccupied) {
+      // console.log('runs  if occupied');
+      return;
+    }
+    var data = div.dataTransfer.getData('text');
+
+    div.target.appendChild(document.getElementById(data));
+  }
