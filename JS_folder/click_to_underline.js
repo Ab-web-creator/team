cd559a51523998db90928968529215edb7a64632
Abdugapir <103257101+Abdugapir@users.noreply.github.com>
2023-02-10 16:33:48 +0100

    lyuboe sobwenie

---
+++ JS_folder/click_to_underline.js
@@ -0,0 +1,14 @@
+const  click_to_underline  = (target_undrln) => {
+    const button = document.querySelector(target_undrln)
+    button.addEventListener("click", () => {
+       button.style.textDecoration = "solid underline purple 4px"
+    })
+   }
+ 
+   click_to_underline(".hebben_form1")
+   click_to_underline(".hebben_form2")
+   click_to_underline(".hebben_form3")
+   click_to_underline(".hebben_form4")
+   click_to_underline(".hebben_form5")
+   click_to_underline(".hebben_form6")
+   click_to_underline(".hebben_form7")
