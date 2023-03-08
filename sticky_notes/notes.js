ec68f4f526a9caa84b81e483ce2f05ebde431959
Abeke Sa <m214@Abekes-MacBook-Pro.local>
2023-03-08 16:54:30 +0100

    lyuboe sobwenie

--- sticky_notes/notes.js
+++ sticky_notes/notes.js
@@ -20,95 +20,97 @@ function saveNotes(notes) {
 }
 
 //creates new note-ELEMENT 
-let numColor;
-function createNoteElement(id, content, numColor, rang_barang) {
+// let numColor;
+function createNoteElement(id, content, numColor) {
 
- const foldercha = document.createElement("div");
- const subFoldercha1 = document.createElement("div");
- const subFoldercha2 = document.createElement("div");
- const varaqcha = document.createElement("textarea");
- const musorka = document.createElement("img");
- musorka.src = `./sticky_notes/trash_can.svg`
+        const foldercha = document.createElement("div");
+        foldercha.classList.add("note");
 
- foldercha.appendChild(subFoldercha1)
- foldercha.appendChild(subFoldercha2)
- subFoldercha1.appendChild(varaqcha)
- subFoldercha2.appendChild(musorka)
+        const subFoldercha1 = document.createElement("div");
 
- foldercha.classList.add("note");
- subFoldercha2.classList.add("subFoldercha2");
+        const subFoldercha2 = document.createElement("div");
+        subFoldercha2.classList.add("subFoldercha2");
 
- musorka.classList.add("trash_red");
-//  musorka.innerHTML = "delete"
-//  let rang_barang;
+        const varaqcha = document.createElement("textarea");
 
-    if ((numColor >= 0) && (numColor < 10)) {
-        rang_barang = "white_varaq";
-    }
-    if ((numColor >= 10) && (numColor < 20)) {
-        rang_barang = "blue_varaq";
-    }
-    if ((numColor >= 20) && (numColor < 25)) {
-        rang_barang = "light_red_varaq";
-    }
-    if ((numColor >= 25) && (numColor < 30)) {
-        rang_barang = "red_varaq";
-    }
-    if ((numColor >= 30) && (numColor < 35)) {
-            rang_barang = "green_varaq";
-    }
-    if ((numColor >= 35) && (numColor < 40)) {
-            rang_barang = "darkgreen_varaq";
-    }
-    if ((numColor >= 40) && (numColor < 50)) {
-            rang_barang = "purple_varaq";
-    }
-    if ((numColor >= 50) && (numColor < 60)) {
-            rang_barang = "golden_varaq";
-    }
-    if ((numColor >= 60) && (numColor < 70)) {
-            rang_barang = "darkBlue_varaq";
-    }
-    if ((numColor >= 70) && (numColor < 80)) {
-            rang_barang = "darkPurple_varaq";
-    }
-    if ((numColor >= 80) && (numColor < 85)) {
-            rang_barang = "pink_varaq";
-    }
-    if ((numColor >= 85) && (numColor < 90)) {
-            rang_barang = "lightpink_varaq";
-    }
-    if ((numColor >= 90) && (numColor < 95)) {
-            rang_barang = "yellow_varaq";
-    }
-    if ((numColor >= 95) && (numColor < 100)) {
-            rang_barang = "black_varaq";
-    }
+        const musorka = document.createElement("img");
+        musorka.src = `./sticky_notes/trash_can.svg`;
+        musorka.classList.add("trash_red");
+
+        foldercha.appendChild(subFoldercha1)
+        foldercha.appendChild(subFoldercha2)
+        subFoldercha1.appendChild(varaqcha)
+        subFoldercha2.appendChild(musorka)
+
+        if ((numColor >= 0) && (numColor < 10)) {
+                rang_ber = "white_varaq";
+        }
+        if ((numColor >= 10) && (numColor < 20)) {
+                rang_ber = "blue_varaq";
+        }
+        if ((numColor >= 20) && (numColor < 25)) {
+                rang_ber = "light_red_varaq";
+        }
+        if ((numColor >= 25) && (numColor < 30)) {
+                rang_ber = "red_varaq";
+        }
+        if ((numColor >= 30) && (numColor < 35)) {
+                rang_ber = "green_varaq";
+        }
+        if ((numColor >= 35) && (numColor < 40)) {
+                rang_ber = "darkgreen_varaq";
+        }
+        if ((numColor >= 40) && (numColor < 50)) {
+                rang_ber = "purple_varaq";
+        }
+        if ((numColor >= 50) && (numColor < 60)) {
+                rang_ber = "golden_varaq";
+        }
+        if ((numColor >= 60) && (numColor < 70)) {
+                rang_ber = "darkBlue_varaq";
+        }
+        if ((numColor >= 70) && (numColor < 80)) {
+                rang_ber = "darkPurple_varaq";
+        }
+        if ((numColor >= 80) && (numColor < 85)) {
+                rang_ber = "pink_varaq";
+        }
+        if ((numColor >= 85) && (numColor < 90)) {
+                rang_ber = "lightpink_varaq";
+        }
+        if ((numColor >= 90) && (numColor < 95)) {
+                rang_ber = "yellow_varaq";
+        }
+        if ((numColor >= 95) && (numColor < 100)) {
+                rang_ber = "black_varaq";
+        }
 
         // varaqcha.classList.add("yellow_varaq");
       
         varaqcha.value = content;
         varaqcha.placeholder = "write here"
+        foldercha.classList.add(rang_ber);
 
         varaqcha.addEventListener("change", () => {
         updateNote(id, varaqcha.value);
         })
 
-        musorka.addEventListener("click", () => {
-        const doDelete = confirm("Are you sure to delete this note?");
-        if (doDelete) {
-                deleteNote(id, foldercha);
-        }
+        subFoldercha2.addEventListener("click", () => {
+                const doDelete = confirm("Are you sure to delete this note?");
+                if (doDelete) {
+                        deleteNote(id, foldercha);
+                }
         })
-
-        varaqcha.classList.add(rang_barang);
 
         return foldercha;
 }
 
+
+
+
 //adds a new note-ELEMENT and saves it in the LS
 function addNote() {
-   
+
     const notes = getNotes();
     const noteObject = {
         id: Math.floor(Math.random() * 1000000),
@@ -123,6 +125,9 @@ function addNote() {
     saveNotes(notes);
 }
 
+
+
+
 //updates changes in the note
 function updateNote(id, newContent) {
     const notes = getNotes();
@@ -135,7 +140,7 @@ function updateNote(id, newContent) {
 //deletes the note
 function deleteNote(id, varaqcha) {
         const notes = getNotes().filter(note => note.id != id);
-        
+
         saveNotes(notes);
         notesContainer.removeChild(varaqcha);
 }
