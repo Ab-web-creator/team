3c14f5224e61aedc459702e2cef202ef8e756ec0
Abeke Sa <m214@Abekes-MacBook-Pro.local>
2023-03-08 12:11:02 +0100

    lyuboe sobwenie

--- sticky_notes/notes.js
+++ sticky_notes/notes.js
@@ -21,7 +21,7 @@ function saveNotes(notes) {
 
 //creates new note-ELEMENT 
 let numColor;
-function createNoteElement(id, content, numColor) {
+function createNoteElement(id, content, numColor, rang_barang) {
 
  const foldercha = document.createElement("div");
  const subFoldercha1 = document.createElement("div");
@@ -35,73 +35,73 @@ function createNoteElement(id, content, numColor) {
  subFoldercha2.appendChild(musorka)
 
  foldercha.classList.add("note");
- varaqcha.classList.add("yellow_varaq");
 
  musorka.classList.add("material-symbols-outlined");
  musorka.innerHTML = "delete"
+//  let rang_barang;
 
     if ((numColor >= 0) && (numColor < 10)) {
-        numColor = "white_varaq";
+        rang_barang = "white_varaq";
     }
     if ((numColor >= 10) && (numColor < 20)) {
-        numColor = "blue_varaq";
+        rang_barang = "blue_varaq";
     }
     if ((numColor >= 20) && (numColor < 25)) {
-        numColor = "light_red_varaq";
+        rang_barang = "light_red_varaq";
     }
     if ((numColor >= 25) && (numColor < 30)) {
-        numColor = "red_varaq";
+        rang_barang = "red_varaq";
     }
     if ((numColor >= 30) && (numColor < 35)) {
-            numColor = "green_varaq";
+            rang_barang = "green_varaq";
     }
     if ((numColor >= 35) && (numColor < 40)) {
-            numColor = "darkgreen_varaq";
+            rang_barang = "darkgreen_varaq";
     }
     if ((numColor >= 40) && (numColor < 50)) {
-            numColor = "purple_varaq";
+            rang_barang = "purple_varaq";
     }
     if ((numColor >= 50) && (numColor < 60)) {
-            numColor = "golden_varaq";
+            rang_barang = "golden_varaq";
     }
     if ((numColor >= 60) && (numColor < 70)) {
-            numColor = "darkBlue_varaq";
+            rang_barang = "darkBlue_varaq";
     }
     if ((numColor >= 70) && (numColor < 80)) {
-            numColor = "darkPurple_varaq";
+            rang_barang = "darkPurple_varaq";
     }
     if ((numColor >= 80) && (numColor < 85)) {
-            numColor = "pink_varaq";
+            rang_barang = "pink_varaq";
     }
     if ((numColor >= 85) && (numColor < 90)) {
-            numColor = "lightpink_varaq";
+            rang_barang = "lightpink_varaq";
     }
     if ((numColor >= 90) && (numColor < 95)) {
-            numColor = "yellow_varaq";
+            rang_barang = "yellow_varaq";
     }
     if ((numColor >= 95) && (numColor < 100)) {
-            numColor = "black_varaq";
+            rang_barang = "black_varaq";
     }
 
+        // varaqcha.classList.add("yellow_varaq");
+      
+        varaqcha.value = content;
+        varaqcha.placeholder = "write here"
 
- varaqcha.value = content;
- varaqcha.placeholder = "пустой"
+        varaqcha.addEventListener("change", () => {
+        updateNote(id, varaqcha.value);
+        })
 
- varaqcha.addEventListener("change", () => {
-    updateNote(id, varaqcha.value);
- })
+        musorka.addEventListener("click", () => {
+        const doDelete = confirm("Are you sure to delete this note?");
+        if (doDelete) {
+                deleteNote(id, foldercha);
+        }
+        })
 
- musorka.addEventListener("click", () => {
-    const doDelete = confirm("Are you sure to delete this note?");
-    if (doDelete) {
-        deleteNote(id, foldercha);
-    }
- })
+        varaqcha.classList.add(rang_barang);
 
- varaqcha.classList.add(numColor);
- console.log(numColor)
-
- return foldercha;
+        return foldercha;
 }
 
 //adds a new note-ELEMENT and saves it in the LS
@@ -114,7 +114,7 @@ function addNote() {
         numColor: Math.floor(Math.random() * 100)
         };
 
-    const noteElement = createNoteElement(noteObject.id, noteObject.content);
+    const noteElement = createNoteElement(noteObject.id, noteObject.content, noteObject.numColor);
     notesContainer.insertBefore(noteElement, addNoteButton);
 
     notes.push(noteObject);
